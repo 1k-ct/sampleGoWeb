10e057c615e206d231739fb3cbff5ad9d5663ae1
1k-ct <268iop@gmail.com>
2020-07-29 16:39:53 +0900

    main

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	user "./service"
+	"github.com/1k-ct/sampleGoWeb/db"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	var s user.Service
+	todos, _ := s.GetAll()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world go gin")
+		c.String(http.StatusOK, "", todos)
+	})
+	db.Init()
+	r.Run()
+	db.Close()
+}
