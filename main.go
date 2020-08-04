324b1ab4129d414c946e7c035edcae99629ffe7d
1k-ct <268iop@gmail.com>
2020-08-04 22:08:38 +0900

    main server

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"net/http"
+	crand "crypto/rand"
+	"errors"
+	"fmt"
+	"math"
+	"math/big"
+	"math/rand"
+	"time"
 
-	user "./service"
-	"github.com/1k-ct/sampleGoWeb/db"
-	"github.com/gin-gonic/gin"
+	_ "github.com/1k-ct/sampleGoWeb/db"
+	_ "github.com/1k-ct/sampleGoWeb/server"
+	_ "github.com/gin-gonic/gin"
 )
 
+/*
 func main() {
 	r := gin.Default()
 	var s user.Service
@@ -19,4 +26,77 @@ func main() {
 	db.Init()
 	server.Init()
 	defer db.Close()
+}
+*/
+func myShuffle(nums ...interface{}) interface{} {
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+	return nums
+}
+
+//
+func gacha() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(100)
+}
+
+// pureGacha 純粋らしい、おそらく
+func pureGacha(n int) int {
+	seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
+	rand.Seed(seed.Int64())
+	return rand.Intn(n)
+}
+func GachaProbabilityMachine() (int, error) {
+	n := gacha()
+	if n < 10 {
+		return n, nil
+	} else if n < 20 {
+		return n, nil
+	} else if n < 100 {
+		return n, nil
+	}
+	return 0, errors.New("Unexpected numbers")
+}
+func main() {
+	nums := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+	fmt.Println(nums)
+	fmt.Println(myShuffle(nums))
+	randN := gacha()
+	fmt.Println(randN)
+	randN = gacha()
+	fmt.Println(randN)
+	monster := []Monster{
+		{
+			rarity: 5, // ★★★★★
+			prob:   0.01,
+			ids: []int{
+				5001,
+				5002,
+				5003,
+			},
+		},
+		{
+			rarity: 6, // ★★★★★★
+			prob:   0.1,
+			ids: []int{
+				6001,
+				6002,
+				6003,
+			},
+		},
+	}
+	fmt.Println(monster[0].ids)
+	ans, _ := GachaProbabilityMachine()
+	fmt.Println(ans)
+}
+
+type id struct{ id []int }
+type Monster struct {
+	rarity int     // ★★★★★
+	prob   float64 // 確立
+	ids    []int
 }
